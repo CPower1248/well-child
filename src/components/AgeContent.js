ba1a4fd53e41389016554126f972bfb557141c23
corey-power <CPower1248@gmail.com>
2021-07-27 23:53:03 -0400

    Added categories

--- src/components/AgeContent.js
+++ src/components/AgeContent.js
@@ -1,8 +1,10 @@
 function AgeContent() {
   return (
     <div className="age-content-container">
-      <div className="age-content">
-        <span>Hello World</span>
+      <div className="age-cat-container">
+        <div className="age-cat-1">Cat 1</div>
+        <div className="age-cat-2">Cat 2</div>
+        <div className="age-cat-3">Cat 3</div>
       </div>
     </div>
   )
