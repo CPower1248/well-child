71fabb0e0716cfeddb86ebcbc06f3ebd01a8dabf
corey-power <CPower1248@gmail.com>
2021-09-17 21:09:29 -0400

    Set default Carousel pics

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,3 +1,6 @@
+import smileyFace from "../images/smiley-face.png"
+import sadFace from "../images/sad-face.png"
+
 import styled from "styled-components"
 
 const CarouselContainer = styled.div`
@@ -19,7 +22,6 @@ const ImgMain = styled.img`
 
   border-radius: 50%;
   box-shadow: 0px 15px 25px -4px rgba(30,30,60,0.5);
-  border: 1px solid black;
 `
 
 const ImgSec = styled.img`
@@ -30,16 +32,15 @@ const ImgSec = styled.img`
 
   border-radius: 50%;
   box-shadow: 0px 15px 25px -4px rgba(30,30,60,0.5);
-  border: 1px solid black;
 `
 
 
 function Carousel() {
   return (
     <CarouselContainer>
-      <ImgSec src="https://picsum.photos/200" alt="Previous Profile" />
-      <ImgMain src="https://picsum.photos/400" alt="Current Profile" />
-      <ImgSec src="https://picsum.photos/200" alt="Next Profile" />
+      <ImgSec src={sadFace} alt="Previous Profile" />
+      <ImgMain src={smileyFace} alt="Current Profile" />
+      <ImgSec src={sadFace} alt="Next Profile" />
     </CarouselContainer>
   )
 }
