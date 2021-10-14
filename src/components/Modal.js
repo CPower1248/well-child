d141252bdf773fbf0b4198c476776b1d584df7a7
corey-power <CPower1248@gmail.com>
2021-10-13 23:29:16 -0400

    ...

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,6 +4,9 @@ import { connect } from "react-redux"
 
 import { toggleModal } from "../actions"
 
+import smiley from "../images/sad-face.png"
+
+// Modal
 const ModalBG = styled.div`
   display: flex;
 	justify-content: center;
@@ -18,15 +21,80 @@ const ModalBG = styled.div`
 
 const ModalContent = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-flow: column;
 
   width: 70%;
 	height: 50%;
 	background-color: rgba(0, 0, 0, 0.975);
 
 	border-radius: 15px;
-  border: 3px double rgb(100, 200, 255);
+  border: 4px double rgb(100, 200, 255);
+`
+
+const CloseButton = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+
+  height: 10%;
+
+  /* border: 1px solid red; */
+
+  button {
+    color: rgb(100, 200, 255);
+    font-weight: bold;
+    font-size: 1rem;
+
+    margin-right: 2%;
+
+    background: none;
+    border: none;
+
+    transition: all 0.5s ease-in-out; 
+
+    &:hover {
+      transform: scale(1.3);
+      transition: all 0.5s ease-in-out; 
+
+      cursor: pointer;
+    }
+  }
+`
+
+const MenuOptions = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  /* border: 1px solid blue; */
+`
+
+const Option = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+
+  text-align: center;
+
+  border: 1px solid blue;
+  border-radius: 15px;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.5s ease-in-out; 
+
+    cursor: pointer;
+  }
+
+  img {
+    border: 1px solid red;
+  }
+
+  span {
+    color: rgb(100, 200, 255);
+
+    border: 1px solid red;
+  }
 `
 
 function Modal({ toggleModal, modal }) {
@@ -45,7 +113,23 @@ function Modal({ toggleModal, modal }) {
   return (
     <ModalBG ref={modalRef}>
       <ModalContent>
-        <button onClick={handleToggleModal}>close</button>
+        <CloseButton>
+          <button onClick={handleToggleModal}>X</button>
+        </CloseButton>
+        <MenuOptions>
+          <Option>
+            <img src={smiley} alt="Your Account" />
+            <span>Your Account</span> 
+          </Option>
+          <Option>
+            <img src={smiley} alt="About Us" />
+            <span>About Us</span> 
+          </Option>
+          <Option>
+            <img src={smiley} alt="Contect Info" />
+            <span>Contact Info</span> 
+          </Option>
+        </MenuOptions>
       </ModalContent>
     </ModalBG>
   )
